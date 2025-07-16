e146018d3e080414190be46238d4b8fd46f851ac
Your Name <you@example.com>
2025-07-16 10:50:46 +0530

    Logo Fixed

--- apps/client/src/pages/Home.tsx
+++ apps/client/src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router';
+import logo from '../assets/HexawareBlueLogo 2.png';
 import Footer from '../components/common/Footer';
 import Bento from '../components/home/Bento';
 import Hero from '../components/home/Hero';
@@ -9,6 +10,9 @@ const Home: React.FC = () => {
 
   return (
     <div className="min-h-screen flex flex-col bg-gray-50">
+      <div className="flex pt-10 pb-4">
+        <img src={logo} alt="Logo" className="h-10 w-auto pl-10" />
+      </div>
       <Hero onGetStarted={() => navigate('/auth')} />
       <Bento />
       <Footer />
