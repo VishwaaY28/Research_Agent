43e019c17a278542758e84151351de9d5f7f0c9b
VishwaaY28 <2000163829@hexaware.com>
2025-09-23 10:18:32 +0530

    Update vite.config.ts

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -5,7 +5,7 @@ import wasm from 'vite-plugin-wasm';
 export default defineConfig({
   plugins: [react(), wasm()],
   server: {
-    host: '0.0.0.0'
+    host: '0.0.0.0',
     port: 8501,
     proxy: {
       '/api': 'http://localhost:8000',
